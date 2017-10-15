277b7db7c6daafc718e88023a84ed9a12cb467e1
Overtorment <overtorment@gmail.com>
2017-10-15 23:53:43 +0300

    init

---
+++ cli-cold-wallet.js
@@ -0,0 +1,20 @@
+var qrcode = require('qrcode-terminal')
+var bitcore = require('bitcore-lib')
+var Table = require('cli-table')
+
+var privateKey = new bitcore.PrivateKey()
+var address = new bitcore.Address(privateKey.toPublicKey())
+var displayWif = privateKey.toWIF()
+var displayAddress = address.toString()
+
+qrcode.generate(displayWif, {small: true}, function (wifCode) {
+  qrcode.generate(displayAddress, {small: true}, function (addressCode) {
+    var table = new Table({head: ['BITCOIN PUBLIC ADDRESS', 'BITCOIN PRIVATE KEY (WIF)']})
+    table.push(
+        [addressCode, wifCode]
+      , ['DEPOSIT / VERIFY', 'WITHDRAW']
+      , [displayAddress, displayWif]
+    )
+    console.log(table.toString())
+  })
+})
